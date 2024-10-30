2e3bf7d72ae4d2835671a2a4bd25d9b8432bfeb7
InGwonKang <tnqkr3494@naver.com>
2024-10-30 14:56:59 +0900

    build: dangerously-set-html-content 설치 및 script 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import InnerHTML from "dangerously-set-html-content";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [viewTerm, setViewTerm] = useState("");
   const [content, setContent] = useState("");
 
   const handleSearch = async () => {
@@ -23,9 +25,26 @@ function App() {
     }
   };
 
+  const handleURLSearch = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/view=${viewTerm}`);
+      const data = await response.text();
+
+      let bodyContent = data.split("\r\n\r\n")[1];
+      bodyContent = bodyContent
+        .replace(/(\r\n)?[0-9a-fA-F]+\r\n/g, "")
+        .slice(0, -1);
+
+      setContent(bodyContent);
+    } catch (error) {
+      console.error("Error fetching HTML:", error);
+      setContent("Failed to fetch content.");
+    }
+  };
+
   const handleRefresh = () => {
-    // 검색어 및 콘텐츠 초기화
     setSearchTerm("");
+    setViewTerm("");
     setContent("");
   };
 
@@ -39,6 +58,8 @@ function App() {
         <h1 className="text-2xl md:text-3xl font-semibold text-gray-800 mb-6">
           Google Search Interface
         </h1>
+
+        {/* 검색용 섹션 */}
         <div className="flex w-full max-w-md gap-2 mb-6">
           <input
             type="text"
@@ -53,15 +74,34 @@ function App() {
           >
             Search
           </button>
+        </div>
+
+        {/* URL 검색용 섹션 */}
+        <div className="flex w-full max-w-md gap-2 mb-6">
+          <input
+            type="text"
+            value={viewTerm}
+            onChange={(e) => setViewTerm(e.target.value)}
+            placeholder="Enter URL term"
+            className="flex-1 p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-green-500 outline-none"
+          />
           <button
-            onClick={handleRefresh}
+            onClick={handleURLSearch}
             className="px-4 py-2 bg-green-600 text-white rounded-lg shadow-md hover:bg-green-700 transition-colors"
           >
-            Reset
+            URL Search
           </button>
         </div>
+
+        <button
+          onClick={handleRefresh}
+          className="px-4 py-2 bg-red-600 text-white rounded-lg shadow-md hover:bg-red-700 transition-colors"
+        >
+          Reset
+        </button>
       </div>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+
+      {content ? <InnerHTML html={content} /> : null}
     </>
   );
 }
